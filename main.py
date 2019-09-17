a042d63abd09080f3ba973d6aa6790de11896b16
huda10 <huda.ragheb10@gmail.com>
2019-09-17 22:21:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, World!"
-    
-@app.route("/salvador")
-def salvador():
-    return "Hello, Salvador"
+@app.route("/save", methods = ['GET'])
+def save():
+    matchid = request.args.get('matchid')
+    kills = float(request.args.get('kills'))
+    deaths = float(request.args.get('deaths'))
+    assists = float(request.args.get('assists'))
+    last_hits = float(request.args.get('last_hits'))
+    denies = float(request.args.get('denies'))
+    gold_per_min = float(request.args.get('gold_per_min'))
+    xp_per_min = float(request.args.get('xp_per_min'))
+    time = float(request.args.get('time'))
+    return "Information is saved successfully."
     
 if __name__ == "__main__":
     app.debug = True
